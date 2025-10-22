2f64dba1d0418ff68b68d70e34e2bab906ab2e65
Andrew Sears <andrew@mayfifteenth.com>
2025-10-22 13:55:24 -0500

    Add Metable package

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Kodeine\Metable\Metable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, HasRoles, HasUlids, Notifiable;
+    use HasFactory, HasRoles, HasUlids, Metable, Notifiable;
 
     /**
      * The attributes that are mass assignable.
